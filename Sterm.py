a9d27e6c5fdcfe9e61995d5746986424897d9f7f
Ryo Matsumoto <bussorenre0088@gmail.com>
2013-05-21 01:40:49 +0900

    input command

--- Sterm.py
+++ Sterm.py
@@ -15,38 +15,54 @@ import Queue
 import time
 
 
-def enque_output(outpipe,q):
+def enque_stdout(outpipe,q):
     while(True):
         for line in iter(lambda:outpipe.readline(), b''):
             q.put(line)
-        #print outpipe.read(1),
 
-def input_command(inpipe):
-    time.sleep(1)
-    commands = ["ls","pwd","ls -la","ruby -v","exit"]
-    for command in commands:
-        inpipe.write(command)
-        inpipe.write("\n")
-        time.sleep(1)
+def enque_stderr(errpipe,q):
+    while(True):
+        for line in iter(lambda:errpipe.read(1), b''):
+            q.put(line)
 
 
 class StermCommand(sublime_plugin.TextCommand):
-    def updateview(self):
-        while(True):
-            try:  line = self.qe.get_nowait() # or q.get(timeout=.1)
-            except Queue.Empty:
-                break
-            else: # got line
-                self.view.insert(self.edit, self.view.size(), line)
+    def inputcommand(self):
+        if(self.pos > self.view.size()):
+            for i in range(self.pos -self.view.size()):
+                self.pos -= 1
+                print "\b"
 
+        while (self.pos < self.view.size()):
+            c = self.view.substr(self.pos)
+            if (c == '\n'):
+                self.inputflag = False
+            self.process.stdin.write(c)
+            self.pos += 1
+
+        sublime.set_timeout(self.inputcommand,100)
+
+    def updateview(self):
         while(True):
             try:  line = self.qo.get_nowait() # or q.get(timeout=.1)
             except Queue.Empty:
                 break
             else: # got line
                 self.view.insert(self.edit, self.view.size(), line)
-
+                self.pos = self.view.size()
+        while(True):
+            try:  line = self.qe.get_nowait() # or q.get(timeout=.1)
+            except Queue.Empty:
+                break
+            else: # got line
+                if(self.inputflag == False):
+                    self.view.insert(self.edit, self.view.size(), line)
+                    self.pos = self.view.size()
+        self.inputflag = True
         sublime.set_timeout(self.updateview,100)
+
+    def firstoutput(self):
+        self.inputflag = False
 
     def run(self, edit):
 #        term = TermView("Test for sTerm")
@@ -56,25 +72,26 @@ class StermCommand(sublime_plugin.TextCommand):
         args = shlex.split(shell_path)
         process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 
+        self.process = process
         self.qo = Queue.Queue()
         self.qe = Queue.Queue()
         self.edit = edit
         self.view = view
+        self.pos = view.size()
+        self.inputflag = False
 
-        to = threading.Thread(target=enque_output,args=(process.stdout,self.qo,))
+        to = threading.Thread(target=enque_stdout,args=(process.stdout,self.qo,))
         to.deamon = True
         to.start()
 
-        te = threading.Thread(target=enque_output,args=(process.stderr,self.qe,))
+        te = threading.Thread(target=enque_stderr,args=(process.stderr,self.qe,))
         te.deamon = True
         te.start()
 
-        ti = threading.Thread(target=input_command,args=(process.stdin,))
-        ti.deamon = True
-        ti.start()
-
         # setTimeout
         sublime.set_timeout(self.updateview,1)
+        sublime.set_timeout(self.firstoutput,1)
+        sublime.set_timeout(self.inputcommand,2)
 
 
 class TermView(object):
