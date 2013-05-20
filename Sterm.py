d038ead85efbbe1eae3b53f1e6f1f094b8b74ac9
Ryo Matsumoto <bussorenre0088@gmail.com>
2013-05-20 18:52:29 +0900

    Shell prompt done

--- Sterm.py
+++ Sterm.py
@@ -8,10 +8,8 @@ This plugin is for Sublime Text 2
 
 import sublime
 import sublime_plugin
+import shlex
 import subprocess
-import os
-import sys
-import re
 
 class StermCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -22,8 +20,7 @@ class TermView(object):
     def __init__(self,name):
         self.name = name
         self.view = None
-        self.pipe = None
-        self.process = subprocess.Popen("cat ./", stdout=subprocess.PIPE)
+        self.process = None
 
     def open(self):
         if self.view is None:
@@ -34,12 +31,19 @@ class TermView(object):
         self.view.set_name(self.name)
         self.view.set_scratch(True)
         self.view.set_read_only(True)
-        edit = self.view.begin_edit("sterm")
-        self.view.insert(edit, self.view.size(), self.process.stdout)
-        end_edit(edit)
+#        edit = self.view.begin_edit("sterm")
+#        self.view.insert(edit, self.view.size(), self.process.stdout)
+#        end_edit(edit)
 
 class StermdoCommand(sublime_plugin.TextCommand):
     def run(self,edit):
-        self.view.insert(edit, self.view.size(), "Hello, World!")
-
-
+        args = shlex.split("/bin/bash")
+        self.process = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=False)
+        self.process.stdin.write("cat\n")
+#        output = self.process.stdout.readline()
+#        self.view.insert(edit, self.view.size(), output)
+        self.process.stdin.write("pwd\n")
+        output = self.process.stdout.readline()
+        self.view.insert(edit, self.view.size(), output)
+        self.view.sel().add(sublime.Region(self.view.size()))
+        self.process.terminate()
