322d8edeed061332ab18ad7e223ceb557656ab75
Ryo Matsumoto <bussorenre0088@gmail.com>
2013-05-21 00:19:09 +0900

    とりあえず動いたヒャッハー

--- Sterm.py
+++ Sterm.py
@@ -10,11 +10,72 @@ import sublime
 import sublime_plugin
 import shlex
 import subprocess
+import threading
+import Queue
+import time
+
+
+def enque_output(outpipe,q):
+    while(True):
+        for line in iter(lambda:outpipe.readline(), b''):
+            q.put(line)
+        #print outpipe.read(1),
+
+def input_command(inpipe):
+    time.sleep(1)
+    commands = ["ls","pwd","ls -la","ruby -v","exit"]
+    for command in commands:
+        inpipe.write(command)
+        inpipe.write("\n")
+        time.sleep(1)
+
 
 class StermCommand(sublime_plugin.TextCommand):
+    def updateview(self):
+        while(True):
+            try:  line = self.qe.get_nowait() # or q.get(timeout=.1)
+            except Queue.Empty:
+                break
+            else: # got line
+                self.view.insert(self.edit, self.view.size(), line)
+
+        while(True):
+            try:  line = self.qo.get_nowait() # or q.get(timeout=.1)
+            except Queue.Empty:
+                break
+            else: # got line
+                self.view.insert(self.edit, self.view.size(), line)
+
+        sublime.set_timeout(self.updateview,100)
+
     def run(self, edit):
-        term = TermView("Test for sTerm")
-        term.open()
+#        term = TermView("Test for sTerm")
+#        term.open()
+        view = sublime.active_window().new_file()
+        shell_path = "/bin/bash --login -i"
+        args = shlex.split(shell_path)
+        process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+        self.qo = Queue.Queue()
+        self.qe = Queue.Queue()
+        self.edit = edit
+        self.view = view
+
+        to = threading.Thread(target=enque_output,args=(process.stdout,self.qo,))
+        to.deamon = True
+        to.start()
+
+        te = threading.Thread(target=enque_output,args=(process.stderr,self.qe,))
+        te.deamon = True
+        te.start()
+
+        ti = threading.Thread(target=input_command,args=(process.stdin,))
+        ti.deamon = True
+        ti.start()
+
+        # setTimeout
+        sublime.set_timeout(self.updateview,1)
+
 
 class TermView(object):
     def __init__(self,name):
@@ -30,20 +91,5 @@ class TermView(object):
         self.view = sublime.active_window().new_file()
         self.view.set_name(self.name)
         self.view.set_scratch(True)
-        self.view.set_read_only(True)
-#        edit = self.view.begin_edit("sterm")
-#        self.view.insert(edit, self.view.size(), self.process.stdout)
-#        end_edit(edit)
+        # self.view.set_read_only(True)
 
-class StermdoCommand(sublime_plugin.TextCommand):
-    def run(self,edit):
-        args = shlex.split("/bin/bash")
-        self.process = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=False)
-        self.process.stdin.write("cat\n")
-#        output = self.process.stdout.readline()
-#        self.view.insert(edit, self.view.size(), output)
-        self.process.stdin.write("pwd\n")
-        output = self.process.stdout.readline()
-        self.view.insert(edit, self.view.size(), output)
-        self.view.sel().add(sublime.Region(self.view.size()))
-        self.process.terminate()
